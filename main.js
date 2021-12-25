85dbd8cfba87ddd2efff8a8e09af65b11766bff2
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-12-25 11:23:33 +0100

    Fix layout to match the figma design

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ const logo = document.getElementById('logo');
 const toggleMenu = () => {
   if (menu.style.display === 'none') {
     menu.style.display = 'block';
+    menu.style.height = '100vh';
     logo.style.display = 'none';
     toggleButton.childNodes[1].className = 'fas fa-times';
+    toggleButton.childNodes[1].style.color = 'grey';
   } else {
     logo.style.display = 'block';
     menu.style.display = 'none';
     toggleButton.childNodes[1].className = 'fas fa-bars';
+    toggleButton.childNodes[1].style.color = '#fff';
   }
 };
-
-toggleMenu();
 
 toggleButton.addEventListener('click', toggleMenu);
